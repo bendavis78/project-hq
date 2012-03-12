8a8d4520896f423eda07ac5fc0896850b2680ea0
Ben Davis <bendavis78@gmail.com>
2012-03-12 17:51:04 -0500

    ordering fixes, added clean_ordering manager method

--- orderable/models.py
+++ orderable/models.py
@@ -1,16 +1,43 @@
-from django.db.models import F
+from django.db import models
 import sys
 
-class OrderableModel(object):
+class OrderableManager(models.Manager):
+    _cleaning = False
+    def clean_ordering(self):
+        if self._cleaning:
+            return
+        self._cleaning = True
+        objects = self.get_query_set()
+        Model = objects.model
+        f = Model.ordering_field
+        objects = self.filter(**{'%s__isnull' % f:False})
+        i = 1
+        for object in objects.order_by(f):
+            object.auto_order = False
+            setattr(object, f, i)
+            object.save()
+            # if another class has overridden save to change the ordering
+            # value, we just move on to the next one.
+            if getattr(object, f) == i:
+                i += 1
+        self._cleaning = False
+
+class OrderableModel(models.Model):
     ordering_field = 'order'
     order_none_first = False
+    auto_order = True
+    orderable = OrderableManager()
+    _cleaning = False
 
     class Meta:
         abstract = True
 
     def save(self):
-        o = getattr(self, self.ordering_field)
+        if not self.auto_order:
+            return super(OrderableModel, self).save()
+
         f = self.ordering_field
+        o = getattr(self, f)
         Model = self.__class__
         if o:
             setattr(self, f, int(o))
@@ -21,13 +48,15 @@ class OrderableModel(object):
                     current_pos = 0
                 else:
                     current_pos = sys.maxint
+            objects = Model.objects.filter(**{'%s__isnull' % f:False})
             if current_pos > o:
                 # when moving down, increment those between the move
-                Model.objects.filter(**{('%s__lt' % f):current_pos, 
-                        ('%s__gte' % f):o}).update(**{f:F(f)+1})
+                objects.filter(**{('%s__lt' % f):current_pos, 
+                        ('%s__gte' % f):o}).update(**{f:models.F(f)+1})
             elif getattr(current, f) < o:
                 # when moving up, decrement those between the move
-                Model.objects.filter(**{('%s__gt' % f):current_pos,
-                        ('%s__lte' % f):o}).update(**{f:F(f)-1})
+                objects.filter(**{('%s__gt' % f):current_pos,
+                        ('%s__lte' % f):o}).update(**{f:models.F(f)-1})
+            
+        return super(OrderableModel, self).save()
 
-        super(OrderableModel, self).save()
--- taskboard/listeners.py
+++ taskboard/listeners.py
@@ -6,3 +6,4 @@ from . import models
 @receiver(post_save, sender=models.Task)
 def update_task_iterations(sender, **kwargs):
     clear_iteration_cache()
+    models.Task.orderable.clean_ordering()
