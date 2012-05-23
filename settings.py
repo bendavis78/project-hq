b9589a3e0f83b739573aa1f423e75ce7404abc14
Ben Davis <bendavis78@gmail.com>
2012-05-22 21:12:46 -0500

    removing company-specific stuff for open-sourcing

---
+++ settings.py
@@ -0,0 +1,9 @@
+# These will be configurable later through admin
+TASKBOARD_ITERATION_DAYS = 7
+TASKBOARD_ITERATION_VELOCITY = 20
+TICKETS_ACTIVITY_WARNING_HOURS = (
+    ('NEW', 12),
+    ('ASSIGNED', 24),
+    ('ACKNOWLEGED', 48),
+    ('FEEDBACK', 72),
+)
